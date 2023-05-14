8e6e4ae97bffb79679c60fe16a9ac01a197feb8b
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-14 21:37:24 +0600

    [ADD]-set-basic-layout-for-footer-and-header

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,15 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main/>,
+    element: <Main />,
     children: [
-        {
-            
-        }
-    ]
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <div className="max-w-7xl mx-auto px-4">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
 );
