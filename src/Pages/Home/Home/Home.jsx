39ee8b3d6eb3440c0b8e0703646d74b146108f97
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-14 22:39:29 +0600

    [ADD]-style-header-and-create-simple-banner

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,11 @@
+import Header from "../Header/Header";
+
 const Home = () => {
-  return <div>This is home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
