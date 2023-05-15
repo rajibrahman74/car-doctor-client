dff0308ef6f6f25c7e10c19366d0775558f897b9
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-15 17:27:03 +0600

    [ADD]-load-services-fake-data-and-display-service-card

---
+++ src/Pages/Home/Services/Service.jsx
@@ -0,0 +1,19 @@
+const Service = ({ service }) => {
+  const { title, img, price } = service;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={img} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center">
+        <h2 className="card-title me-auto">{title}</h2>
+        <p className="text-error font-semibold me-auto">Price: ${price}</p>
+        <div className="card-actions me-auto">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import Service from "./Service";
+
+const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+  return (
+    <div>
+      <div>
+        <h3 className="font-extrabold text-xl text-error text-center">
+          About Us
+        </h3>
+        <h1 className="text-5xl font-extrabold text-center">
+          Our Service Area
+        </h1>
+        <p className="text-center mx-72 pt-5">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which dont look even slightly believable.{" "}
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 py-10">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
