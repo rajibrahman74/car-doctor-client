482bfc070ad1cbe2373d92f8ae48c7566d3085c9
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-17 21:21:57 +0600

    [ADD]-send-jwt-token-in-the-server-verify-and-decode-jwt-token

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -10,7 +10,12 @@ const Bookings = () => {
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
 
   useEffect(() => {
-    fetch(url)
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("access token")}`
+      }
+    })
       .then((res) => res.json())
       .then((data) => setBookings(data));
   }, [url]);
