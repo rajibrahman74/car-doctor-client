11622acb47e09d976550dc1c500a26fcbdecffb1
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-23 19:06:30 +0600

    [ADD]-replace-server-side-to-localhost

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -4,7 +4,7 @@ import Service from "./Service";
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch("https://car-doctor-server-ten-drab.vercel.app/services")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
