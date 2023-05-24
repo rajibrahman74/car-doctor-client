934aa5d762f5c37634f6d2e7c83bccb7395f7b39
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-24 17:30:52 +0600

    [ADD]-price-range-100-to-300-and-Mongodb-operators

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -3,11 +3,12 @@ import Service from "./Service";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [asc, setAsc] = useState(true);  // price low hight state
   useEffect(() => {
-    fetch("http://localhost:5000/services")
+    fetch(`http://localhost:5000/services?sort=${asc ? "asc" : "desc"}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
-  }, []);
+  }, [asc]);
   return (
     <div>
       <div>
@@ -21,6 +22,12 @@ const Services = () => {
           the majority have suffered alteration in some form, by injected
           humour, or randomised words which dont look even slightly believable.{" "}
         </p>
+        <button
+          onClick={() => setAsc(!asc)}
+          className="btn btn-secondary text-white flex justify-center items-center mx-auto my-4"
+        >
+          {asc ? "Price High to low" : "Price Low to hight"}
+        </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-10 py-10">
         {services.map((service) => (
